d4089ec53f5d7a4f9ad7f085bde78a9413ae9f36
Manoel Ribeiro <manoelribeiro20181@gmail.com>
2020-05-18 19:52:57 -0300

    Entidade Worker finalizada

---
+++ ConsoleApp1/Entiety/Enum/WorkerLevel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Work.Entiety.Enum
+{
+    enum WorkerLevel : int
+    {
+        Junior = 0,
+        Mid_Level = 1,
+        Senior = 2
+    }
+}
---
+++ ConsoleApp1/Entiety/Worker.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Work.Entiety.Enum;
+using Work.Entiety;
+
+namespace Work.Entiety
+{
+    class Worker
+    {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Department department { get; set; }
+        public List<HourContract> Contracts { get; set; } = new List<HourContract>();
+
+        public Worker()
+        {
+
+        }
+
+        public Worker(string name, WorkerLevel level, double baseSalary, Department department)
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+            this.department = department;
+        }
+
+        public void AddContracts(HourContract contract)
+        {
+            Contracts.Add(contract);
+        }
+
+        public void RemoveContracts(HourContract contract)
+        {
+            Contracts.Remove(contract);
+        }
+
+        public double Income(int month, int year)
+        {
+            double sum = BaseSalary;
+            foreach(HourContract hourContract in Contracts)
+            {
+                if(hourContract.Date.Year == year && hourContract.Date.Month == month)
+                {
+                    sum += hourContract.TotalValue();
+                }
+            }
+            return sum;
+
+        }
+    }
+}
